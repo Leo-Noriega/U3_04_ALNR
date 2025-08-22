72b0c6ea7183eb98d4f6d928427a0b79bd603fd8
Leo-Noriega <leonoriega100@gmail.com>
2025-08-22 15:34:11 -0600

    Initialize project with Maven setup, including wrapper scripts, POM file, and basic Spring Boot application structure. Added .gitignore and .gitattributes for configuration management.

---
+++ src/main/java/mx/edu/utez/u3_04_jggj/U304JggjApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.u3_04_jggj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U304JggjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(U304JggjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/edu/utez/u3_04_jggj/U304JggjApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.u3_04_jggj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U304JggjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
